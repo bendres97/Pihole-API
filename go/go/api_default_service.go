5f2705b33efa9e0c25dea24e8a94d5cc9ddb432f
Bryan Endres <bryan@endres.io>
2023-06-12 20:52:52 -0500

    Implement Gravity GET
    Signed-off-by: Bryan Endres <bryan@Gaming-Rig.localdomain>

--- go/go/api_default_service.go
+++ go/go/api_default_service.go
@@ -10,9 +10,14 @@
 package openapi
 
 import (
+	"bytes"
 	"context"
-	"net/http"
 	"errors"
+	"log"
+	"net/http"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 // DefaultAPIService is a service that implements the logic for the DefaultAPIServicer
@@ -26,18 +31,47 @@ func NewDefaultAPIService() DefaultAPIServicer {
 	return &DefaultAPIService{}
 }
 
-// GravityGet - 
+// GravityGet -
 func (s *DefaultAPIService) GravityGet(ctx context.Context) (ImplResponse, error) {
 	// TODO - update GravityGet with the required logic for this service method.
 	// Add api_default_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
 
-	// TODO: Uncomment the next line to return response Response(200, []GravityObj{}) or use other options such as http.Ok ...
-	// return Response(200, []GravityObj{}), nil
+	cmd := exec.Command("/usr/bin/sqlite3", "/etc/pihole/gravity.db", "SELECT id, address, comment FROM adlist;")
+	output, err := cmd.Output()
+	if err != nil {
+		var errb bytes.Buffer
+		cmd.Stderr = &errb
+		log.Print(cmd.Args)
+		log.Print(string(output))
+		log.Print(errb.String())
+		return Response(500, err.Error()), err
+	}
 
-	return Response(http.StatusNotImplemented, nil), errors.New("GravityGet method not implemented")
+	var gravity []GravityObj
+
+	result := strings.TrimSpace(string(output))
+	lines := strings.Split(result, "\n")
+	for _, line := range lines {
+		split := strings.Split(line, "|")
+		id_val, err := strconv.Atoi(split[0])
+		if err != nil {
+			return Response(500, err.Error()), err
+		}
+		id := int32(id_val)
+		address := split[1]
+		comment := split[2]
+		gravity = append(gravity, GravityObj{
+			Id:      id,
+			Address: address,
+			Comment: comment,
+		})
+	}
+
+	// TODO: Uncomment the next line to return response Response(200, []GravityObj{}) or use other options such as http.Ok ...
+	return Response(200, gravity), err
 }
 
-// GravityIdDelete - 
+// GravityIdDelete -
 func (s *DefaultAPIService) GravityIdDelete(ctx context.Context, id int32) (ImplResponse, error) {
 	// TODO - update GravityIdDelete with the required logic for this service method.
 	// Add api_default_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
@@ -51,7 +85,7 @@ func (s *DefaultAPIService) GravityIdDelete(ctx context.Context, id int32) (Impl
 	return Response(http.StatusNotImplemented, nil), errors.New("GravityIdDelete method not implemented")
 }
 
-// GravityIdPatch - 
+// GravityIdPatch -
 func (s *DefaultAPIService) GravityIdPatch(ctx context.Context, id int32, gravityObj GravityObj) (ImplResponse, error) {
 	// TODO - update GravityIdPatch with the required logic for this service method.
 	// Add api_default_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
@@ -65,7 +99,7 @@ func (s *DefaultAPIService) GravityIdPatch(ctx context.Context, id int32, gravit
 	return Response(http.StatusNotImplemented, nil), errors.New("GravityIdPatch method not implemented")
 }
 
-// GravityPost - 
+// GravityPost -
 func (s *DefaultAPIService) GravityPost(ctx context.Context, gravityObj GravityObj) (ImplResponse, error) {
 	// TODO - update GravityPost with the required logic for this service method.
 	// Add api_default_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
